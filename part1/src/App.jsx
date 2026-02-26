e922a0527add3e2f2f530771f54d7b8d33fbb33a
Clyde Fernandes <clyde@clydes-MacBook-Pro-2.local>
2026-02-26 20:46:29 +0400

    Finished exercise 1.3, refactored the parts into object

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -28,21 +28,27 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course = {course}/>
-      <Content firstPart = {part1} numberOfExercises1 = {exercises1}
-      secondPart = {part2} numberOfExercises2 = {exercises2}
-      thirdPart = {part3} numberOfExercises3 = {exercises3}
+      <Content firstPart = {part1.name} numberOfExercises1 = {part1.exercises}
+      secondPart = {part2.name} numberOfExercises2 = {part2.exercises}
+      thirdPart = {part3.name} numberOfExercises3 = {part3.exercises}
       />
-      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
+      <Total exercises1 = {part1.exercises} exercises2 = {part2.exercises} exercises3 = {part3.exercises}/>
     </div>
   );
 }
