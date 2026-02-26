7c6817a6c3596d53a3afc070758563ddb02830dd
Clyde Fernandes <clyde@clydes-MacBook-Pro-2.local>
2026-02-26 21:10:00 +0400

    Redid exercise 1.4 with added constraint that the components be made to match the array

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -7,9 +7,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part = {props.firstPart} numberOfExercises = {props.numberOfExercises1}/>
-      <Part part = {props.secondPart} numberOfExercises = {props.numberOfExercises2}/>
-      <Part part = {props.thirdPart} numberOfExercises = {props.numberOfExercises3}/>
+      <Part part = {props.parts[0].name} numberOfExercises = {props.parts[0].exercises}/>
+      <Part part = {props.parts[1].name} numberOfExercises = {props.parts[1].exercises}/>
+      <Part part = {props.parts[2].name} numberOfExercises = {props.parts[2].exercises}/>
     </>
   );
 }
@@ -22,7 +22,7 @@ const Part = (props) => {
 
 const Total = (props) => {
   return(
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   );
 }
 
@@ -44,14 +44,12 @@ const App = () => {
   ]
 
 
+
   return (
     <div>
       <Header course = {course}/>
-      <Content firstPart = {parts[0].name} numberOfExercises1 = {parts[0].exercises}
-      secondPart = {parts[1].name} numberOfExercises2 = {parts[1].exercises}
-      thirdPart = {parts[2].name} numberOfExercises3 = {parts[2].exercises}
-      />
-      <Total exercises1 = {parts[0].exercises} exercises2 = {parts[1].exercises} exercises3 = {parts[2].exercises}/>
+      <Content parts = {parts}/>
+      <Total parts={parts}/>
     </div>
   );
 }
