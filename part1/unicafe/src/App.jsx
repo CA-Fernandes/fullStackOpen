02e1c4eed0e6ab574fe14d1dbb32e42824f5faa3
Clyde Fernandes <clyde@clydes-MacBook-Pro-2.local>
2026-03-04 21:29:40 +0400

    Solved exercise 1.7, all statistics are now in single stat components

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,6 +10,11 @@ const Button = (props) => (
   <button onClick={props.onClick}>{props.text}</button>
 )
 
+const Stat = ({text, statistic}) => (
+  <div>{text} {statistic}</div>
+)
+
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -20,6 +25,14 @@ const App = () => {
     setButtonState(state + 1)
     console.log("state after update: ", state)
   }
+
+  const total = good + neutral + bad;
+  const average = total === 0 
+  ? 0 
+  : (good - bad) / total;
+
+  const positivePercentage = good / total
+
   return(
     <div>
       <Header heading = "give feedback"/>
@@ -27,9 +40,12 @@ const App = () => {
       <Button onClick = {() => incrementOnClick(neutral, setNeutral)} text="Neutral"></Button>
       <Button onClick = {() => incrementOnClick(bad, setBad)} text="Bad"></Button>
       <Header heading = "statistics"/>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Stat text="good" statistic={good}/>
+      <Stat text="neutral" statistic={neutral}/>
+      <Stat text="bad" statistic={bad}/>
+      <Stat text="all" statistic={total}/>
+      <Stat text="average" statistic={average}/>
+      <Stat text="positive" statistic={`${positivePercentage}%`}/>
     </div>
   )
 }
