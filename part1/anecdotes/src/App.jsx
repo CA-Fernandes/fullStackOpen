ffb4e820702aad0aab6bee2ca20f3d30c1ad2b3e
Clyde Fernandes <clyde@clydes-MacBook-Pro-2.local>
2026-03-05 17:45:10 +0400

    Finished exercise 1.13 and 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -6,8 +6,22 @@ const Button = (props) => (
   </>
 )
 
-const Anecdote = ({text}) => (
+const Anecdote = (props) => {
+  return(
+  <div>
+    <h1>{props.heading}</h1>
+    <AnecdoteLine text = {props.text}/>
+    <Votes numberOfVotes={props.numberOfVotes}/>
+  </div>
+  )
+}
+
+const AnecdoteLine = ({text}) => (
   <div>{text}</div>
+)
+
+const Votes = ({numberOfVotes}) => (
+  <div>has {numberOfVotes} votes</div>
 )
 const App = () => {
   const anecdotes = [
@@ -20,22 +34,36 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
+  const votes = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0}
   const [selected, setSelected] = useState(0)
+  const [votesForAnecdotes, setVotes] = useState(votes)
+  
 
-  const handleClick = () => {
-    setSelected(getRandomInt(anecdotes.length))
-    console.log(selected)
+  const handleClickChangeAnecdote = () => {
+    const randomInteger = getRandomInt(anecdotes.length)
+    setSelected(randomInteger)
+  }
+  const handleClickUpvoteAnecdote = () => {
+    const votesCopy = {...votesForAnecdotes}
+    votesCopy[selected] += 1
+    setVotes(votesCopy)
   }
 
   const getRandomInt = (max) => {
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored))
   }
+  const getMostVotedIndex = (votesArray) => Object.keys(votesArray).reduce(
+    (a, b) => votesArray[a] > votesArray[b] ? a : b);
+  const mostVotedIndex = getMostVotedIndex(votesForAnecdotes)
+
 
   return (
     <div>
-      <Anecdote text = {anecdotes[selected]}></Anecdote>
-      <Button onClick = {handleClick} text = "next anecdote"/>
+      <Anecdote heading = "Anecdote of the day" text = {anecdotes[selected]} numberOfVotes = {votesForAnecdotes[selected]}/>
+      <Button onClick = {handleClickUpvoteAnecdote} text = "votes"/>
+      <Button onClick = {handleClickChangeAnecdote} text = "next anecdote"/>
+      <Anecdote heading = "Anecdote with most votes" text = {anecdotes[mostVotedIndex]} numberOfVotes = {votesForAnecdotes[mostVotedIndex]}/>
     </div>
   )
 }
